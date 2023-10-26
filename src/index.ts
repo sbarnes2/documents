002e6cf011e08980e0eead29e25b943a801752d0
sbarnes2 <sbarnes2@gmail.com>
2023-10-26 10:02:18 +0100

    re add vue engine

--- src/index.ts
+++ src/index.ts
@@ -16,8 +16,8 @@ const app = express();
 // Configure the application to parse json.
 app.use(express.json());
 // Configure Express to use EJS
-/* app.set( "views", path.join( __dirname, "views" ) );
-app.set( "view engine", "ejs" ); */
+ app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
 
 
 app.use( express.static( path.join( __dirname, "public" ) ) );
